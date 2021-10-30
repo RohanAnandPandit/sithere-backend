c1bee2d1be20dabc7ab6ffe187819b34147bfd84
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-10-30 17:48:40 +0100

    String to represent layout of lecture theatres

---
+++ src/seating/seatingPlans.js
@@ -0,0 +1,17 @@
+import LectureTheatre from "./lectureTheatre";
+
+export var lt1 = `
+    D  A   S S S S S S S S S S S  A  D
+    S S A   S S S S S S S S S S  A S S
+    S S S A  S S S S S S S S S  A S S S
+    S S S S A  S S S S S S S S A S S S S
+    S S S S  A  S S S S S S S A S S S S
+    S S S S   A  S S S S S S A S S S S
+`;
+
+// lt1 = lt1
+//   .trim()
+//   .split("\n")
+//   .map((s) => s.replaceAll(" ", ""));
+
+// console.log(new LectureTheatre(lt1).toString());
