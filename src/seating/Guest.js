67051a5d96f7a680e2c23e21618bc5d55b30eeeb
Pandit, Rohan <rohan.pandit19@imperial.ac.uk>
2021-10-30 17:42:50 +0100

    Class to store details about an external visitor

---
+++ src/seating/Guest.js
@@ -0,0 +1,7 @@
+export default class Guest {
+  constructor({ firstName, lastName }, organisation = null) {
+    this.firstName = firstName;
+    this.lastName = lastNamel;
+    this.organisation = organisation;
+  }
+}
